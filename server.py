ef7f6ae119b80944d80a274c27db82d35c351004
Arijit Layek <arijit.layek1991@gmail.com>
2015-04-26 02:07:55 +0530

    Add the add item functionality

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 app = Flask(__name__)
 
 from sqlalchemy import create_engine
@@ -13,15 +13,20 @@ session = DBSession()
 
 # @app.route('/')
 @app.route('/restaurants/<int:restaurant_id>/')
-def restaurantMenu(restaurant_id):
+def restaurant_menu(restaurant_id):
   restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
   items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
   return render_template('menu.html', restaurant=restaurant, items=items)
   
 #Task 1: Create route for new_menu_item function here
-@app.route('/restaurants/<int:restaurant_id>/new/')
+@app.route('/restaurants/<int:restaurant_id>/new/', methods=['GET', 'POST'])
 def new_menu_item(restaurant_id):
-  return "page to create a new menu item. Task 1 complete!"
+  if request.method == 'POST':
+    new_item = MenuItem(name=request.form['name'], restaurant_id=restaurant_id)
+    session.add(new_item)
+    return redirect(url_for('restaurant_menu', restaurant_id=restaurant_id))
+  else:
+    return render_template('new_menu.html', restaurant_id=restaurant_id)
   
 #Task 2: Create route for edit_menu_item function here
 @app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit/')
