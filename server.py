1eb7185ea4560767f9a5424709a40c3510661664
Arijit Layek <arijit.layek1991@gmail.com>
2015-04-26 00:11:40 +0530

    Remove string from server code. Add templates

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 from sqlalchemy import create_engine
@@ -16,16 +16,7 @@ session = DBSession()
 def restaurantMenu(restaurant_id):
   restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
   items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
-  output = ''
-  for item in items:
-    output += item.name
-    output += '</br>'
-    output += item.price
-    output += '</br>'
-    output += item.description
-    output +='</br>'
-    output +='</br>'
-  return output
+  return render_template('menu.html', restaurant=restaurant, items=items)
   
 #Task 1: Create route for new_menu_item function here
 @app.route('/restaurants/<int:restaurant_id>/new/')
